c1fdc49fc7e5e88a392aff178827f79b4f7a7709
vboulineau <vincent.boulineau@datadoghq.com>
2020-01-20 10:40:28 -0500

    Add container uptime metric for Docker/Containerd/CRI

--- pkg/collector/corechecks/containers/containerd.go
+++ pkg/collector/corechecks/containers/containerd.go
@@ -178,6 +178,7 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 	}
 
 	for _, ctn := range containers {
+		currentTime := time.Now()
 		info, err := cu.Info(ctn)
 		if err != nil {
 			log.Errorf("Could not retrieve the metadata of the container: %s", ctn.ID()[:12])
@@ -211,6 +212,7 @@ func computeMetrics(sender aggregator.Sender, cu cutil.ContainerdItf, fil *ddCon
 			continue
 		}
 
+		computeUptime(sender, info, currentTime, tags)
 		computeMem(sender, metrics.Memory, tags)
 
 		if metrics.CPU.Throttling != nil && metrics.CPU.Usage != nil {
@@ -293,6 +295,13 @@ func computeHugetlb(sender aggregator.Sender, huge []*cgroups.HugetlbStat, tags 
 		sender.Gauge("containerd.hugetlb.max", float64(h.Max), "", tags)
 		sender.Gauge("containerd.hugetlb.failcount", float64(h.Failcnt), "", tags)
 		sender.Gauge("containerd.hugetlb.usage", float64(h.Usage), "", tags)
+	}
+}
+
+func computeUptime(sender aggregator.Sender, ctn containers.Container, currentTime time.Time, tags []string) {
+	uptime := currentTime.Sub(ctn.CreatedAt).Seconds()
+	if uptime > 0 {
+		sender.Gauge("containerd.uptime", uptime, "", tags)
 	}
 }
 
